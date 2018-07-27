494de7244af6090f41459d0619ffe717cef23bcd
lprimat <loic.primat>
2018-07-27 19:56:33 +0200

    Basic player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
 
-public class Player {
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < 2; i++) {
+                int playerHealth = in.nextInt();
+                int playerMana = in.nextInt();
+                int playerDeck = in.nextInt();
+                int playerRune = in.nextInt();
+            }
+            int opponentHand = in.nextInt();
+            int cardCount = in.nextInt();
+            for (int i = 0; i < cardCount; i++) {
+                int cardNumber = in.nextInt();
+                int instanceId = in.nextInt();
+                int location = in.nextInt();
+                int cardType = in.nextInt();
+                int cost = in.nextInt();
+                int attack = in.nextInt();
+                int defense = in.nextInt();
+                String abilities = in.next();
+                int myHealthChange = in.nextInt();
+                int opponentHealthChange = in.nextInt();
+                int cardDraw = in.nextInt();
+            }
+
+
+            System.out.println("PASS");
+        }
+    }
 }
